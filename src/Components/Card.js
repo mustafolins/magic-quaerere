7e1628bf34925f7d1d8027ad7baad7c7b87c6c4c
mustafolins <mustafolins@gmail.com>
2022-09-17 16:18:51 -0600

    Added additional card data.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 
+const imgStyle = {
+    border: '1px solid #ddd',
+    borderRadius: '4px',
+    padding: '5px',
+    width: '95%'
+}
+
+const divStyle = {
+    border: '1px dashed #fff'
+}
+
 export default class Card extends Component {
     constructor(props) {
         super(props)
@@ -10,12 +21,30 @@ export default class Card extends Component {
     }
     render() {
         return (
-            <div>
-                <p>{this.props.name}</p>
-                <p>{this.props.oracle_text}</p>
-                <p>{this.props.flavor_text}</p>
-                <img src={this.state.imgToUse} alt='' />
-            </div>
+            <table style={divStyle}>
+                <tr>
+                    <td>
+                        <p>{this.props.name}</p>
+                    </td>
+                    <td>
+                        <p>CMC: {this.props.cmc}</p>
+                    </td>
+                    <td style={{textAlign: 'left', width: '10%'}}>
+                        <p>USD foil: {(this.props.prices != null && this.props.prices.usd_foil != null) ? this.props.prices.usd_foil : ''}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style={{width: '33%'}}>
+                        <img src={this.state.imgToUse} alt='' style={imgStyle} />
+                    </td>
+                    <td style={{textAlign: 'left'}}>
+                        <p>Oracle Text:</p>
+                        <p>{this.props.oracle_text}</p>
+                        <p>Flavor:</p>
+                        <p>{this.props.flavor_text}</p>
+                    </td>
+                </tr>
+            </table>
         )
     }
 }
--- src/Components/CardSearch.js
+++ src/Components/CardSearch.js
@@ -1,34 +1,65 @@
 import React, { Component } from 'react'
 import Card from './Card';
 
+const liStyle = {
+    padding: '2px'
+}
+
 export default class CardSearch extends Component {
     constructor(props) {
         super(props);
         this.state = {
             cardData: null,
             color: props.color,
-            power: props.power
+            power: props.power,
+            toughness: null
         }
         this.search = this.search.bind(this);
         this.colorChanged = this.colorChanged.bind(this);
         this.powerChanged = this.powerChanged.bind(this);
+        this.toughChanged = this.toughChanged.bind(this);
     }
     colorChanged(event) {
         this.setState({
             cardData: this.state.cardData,
             color: event.target.value,
-            power: this.state.power
+            power: this.state.power,
+            toughness: this.state.toughness
         })
     }
     powerChanged(event) {
         this.setState({
             cardData: this.state.cardData,
             color: this.state.color,
-            power: event.target.value
+            power: event.target.value,
+            toughness: this.state.toughness
+        })
+    }
+    toughChanged(event){
+        this.setState({
+            cardData: this.state.cardData,
+            color: this.state.color,
+            power: this.state.power,
+            toughness: event.target.value
         })
     }
     search() {
-        fetch('https://api.scryfall.com/cards/search?q=c%3A' + this.state.color + '+pow' + encodeURIComponent(this.state.power))
+        let query = 'https://api.scryfall.com/cards/search?q='
+        let hasOthers = false
+        if (this.state.color !== '') {
+            query += encodeURIComponent('c:' + this.state.color)
+            hasOthers = true
+        }
+        if (this.state.power !== '') {
+            query += (hasOthers ? '+' : '') + 'pow' + encodeURIComponent(this.state.power) // might want to use drop-down box for equality comparison
+            hasOthers = true
+        }
+        if (this.state.toughness !== '') {
+            query += (hasOthers ? '+' : '') + 'tou' + encodeURIComponent(this.state.toughness) // might want to use drop-down box for equality comparison
+            hasOthers = true
+        }
+        console.log(query)
+        fetch(query)
             .then(response => {
                 return response.json();
             })
@@ -47,13 +78,15 @@ export default class CardSearch extends Component {
             <div>
                 <input type="text" placeholder='Color' value={(this.state.color != null) ? this.state.color : ''} onChange={this.colorChanged}></input>
                 <input type="text" placeholder='Power' value={(this.state.power != null) ? this.state.power : ''} onChange={this.powerChanged}></input>
+                <input type="text" placeholder='Toughness' value={(this.state.toughness != null) ? this.state.toughness : ''} onChange={this.toughChanged}></input>
 
                 <button onClick={this.search}>Search!</button>
 
                 <ul>
                     {(this.state.cardData == null) ? '' : this.state.cardData.map((card) => (
-                        <li key={card.id}>
-                            <Card name={card.name} oracle_text={card.oracle_text} flavor_text={card.flavor_text} image_uris={card.image_uris} />
+                        <li key={card.id} style={liStyle}>
+                            <Card name={card.name} oracle_text={card.oracle_text} flavor_text={card.flavor_text} image_uris={card.image_uris}
+                            cmc={card.cmc} color_identity={card.color_identity} legalities={card.legalities} prices={card.prices} />
                         </li>
                     ))}
                 </ul>
