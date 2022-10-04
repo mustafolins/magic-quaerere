5112b5aac33090ff2a7c39af1502172e0434d6cc
mustafolins <mustafolins@gmail.com>
2022-10-03 23:57:49 -0600

    Revert "Added router and started battlefield."
    This reverts commit 5877de009857d4ce194d2e1efa78b901b1eee10f.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import CardSearch from './Components/CardSearch';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Routes, Route } from 'react-router-dom';
-import Battlefield from './Components/P2P/Battlefield';
 
 const darkTheme = createTheme({
   palette: {
@@ -18,10 +16,7 @@ function App() {
       <header className="App-header">
         <ThemeProvider theme={darkTheme}>
           <CssBaseline />
-          <Routes>
-            <Route path='/' element={<CardSearch color={['r']} order='name' />} />
-            <Route path='/battle' element={<Battlefield />} />
-          </Routes>
+          <CardSearch color={['r']} order='name' />
         </ThemeProvider>
       </header>
     </div>
